f325a8ca8605ff78407153f1aace3c812a43c94e
Huy Bui <huybui@Huys-Mac-mini.local>
2020-12-09 07:39:36 +0200

    Add tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { title } from 'process';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type AppProps = {
+
+}
+
+type AppState = {
+  title: string
+}
+
+class App extends React.Component<AppProps, AppState> {
+
+  state: AppState = {
+    title: "This is the title"
+  }
+
+  render() {
+    return (
+      <div className="bg-gray-600 text-white p-5 border">{ this.state.title }</div>
+    );
+  }
+
 }
 
 export default App;
