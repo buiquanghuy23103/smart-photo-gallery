b1c25c9e2fc000408412d26535bdc560379681ea
Huy Bui <huybui@Huys-Mac-mini.local>
2020-12-10 10:57:43 +0200

    Toggle image

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,46 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { title } from 'process';
+import Picture from './components/PictureComponent';
 
 type AppProps = {
 
 }
 
 type AppState = {
-  title: string
+  title: string;
+  shouldShowPicture: boolean
 }
 
 class App extends React.Component<AppProps, AppState> {
 
   state: AppState = {
-    title: "This is the title"
+    title: "This is the title",
+    shouldShowPicture: false,
+  }
+
+  toggleImageAppearance = () => {
+    this.setState({
+      shouldShowPicture: !this.state.shouldShowPicture
+    })
   }
 
   render() {
     return (
-      <div className="bg-gray-600 text-white p-5 border">{ this.state.title }</div>
+      <section className="flex justify-center">
+        <div className="w-1/2">
+          <div className="text-center">
+            <div className="my-4"> { this.state.title } </div>
+            <button
+              className="p-1 bg-blue-700 text-white my-2"
+              onClick={this.toggleImageAppearance}
+            >
+              Toggle image
+              </button>
+          </div>
+        </div>
+        <Picture shouldShowPicture={ this.state.shouldShowPicture } />
+      </section>
     );
   }
 
---
+++ src/components/PictureComponent.tsx
@@ -0,0 +1,16 @@
+type PictureProps = {
+    shouldShowPicture: boolean
+};
+
+const Picture: React.FunctionComponent<PictureProps> = (props: PictureProps) => {
+
+    if (!props.shouldShowPicture) return null
+
+    return (
+        <div>
+            <img src="https://images.unsplash.com/photo-1532298229144-0ec0c57515c7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1308&q=80" />
+        </div>
+    );
+}
+
+export default Picture;
