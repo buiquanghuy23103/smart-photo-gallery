845cfffd8d94294f3806b1a84674e2a6dddce1b7
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-01-05 12:10:41 +0200

    Feature: Infinite scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,30 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import './App.css';
 import LoadingComponent from './components/LoadingComponent';
 import { PictureListComponent } from './components/PictureListComponent';
 import useFetchPicture from './utils/hooks/useFetchPicture';
-import useScroll from './utils/hooks/useScroll';
 
 
 function App(): JSX.Element {
 
-  const scrollPosition = useScroll();
   const [page, setPage] = useState(1);
-  const [pictureList, setPictureList, errors, isLoading] = useFetchPicture(page);
+  const [pictureList, setPictureList, errors] = useFetchPicture(page);
 
   function removePicture(pictureId: string) {
     const pictureListClone = [...pictureList];
     const newPictureList = pictureListClone.filter(picture => picture.id !== pictureId);
     setPictureList(newPictureList);
-    console.log("State has just been changed");
-
   }
 
   function loadMore() {
     setPage(page + 1);
   }
 
-  function shouldHideOnErrorsAndLoading() {
-    if (errors.length > 0) {
-      return "hidden";
-    }
-    return ""
-  }
 
   function showErrorMessage() {
     if (errors.length > 0) {
       return (<div className="flex h-screen">
-
         <p className="m-auto">{ errors[0] }</p>
       </div>)
     }
@@ -42,15 +32,7 @@ function App(): JSX.Element {
     return null;
   }
 
-  useEffect(() => {
-    const isBottomReached = scrollPosition === document.body.offsetHeight - window.innerHeight;
-    if (isBottomReached) {
-      loadMore();
-    }
-  }, [scrollPosition]);
 
-
-  if (isLoading) return (<LoadingComponent />)
 
   return (
     <section className="flex justify-center">
@@ -62,10 +44,17 @@ function App(): JSX.Element {
           </div>
         </div>
         <section>
-          <PictureListComponent
-            pictureList={ pictureList }
-            onItemClick={ removePicture } />
-          <button className={ shouldHideOnErrorsAndLoading() } type="submit" onClick={ loadMore }>Load more</button>
+          <InfiniteScroll
+            dataLength={ pictureList.length }
+            next={ loadMore }
+            hasMore={ true }
+            loader={ <LoadingComponent /> }
+          >
+
+            <PictureListComponent
+              pictureList={ pictureList }
+              onItemClick={ removePicture } />
+          </InfiniteScroll>
         </section>
       </div>
 
--- src/utils/hooks/useFetchPicture.tsx
+++ src/utils/hooks/useFetchPicture.tsx
@@ -8,23 +8,19 @@ const PUBLIC_KEY = process.env.REACT_APP_UNSPLASH_PUBLIC_KEY;
 export default function useFetchPicture(pageNumber: number) {
     const [pictureList, setPictureList] = useState([] as Picture[]);
     const [errors, setErrors] = useState([] as string[]);
-    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
-        setIsLoading(true);
         axios.get<Picture[]>(`${BASE_URL}?client_id=${PUBLIC_KEY}&page=${pageNumber}`)
             .then(res => {
                 setPictureList([...pictureList, ...res.data]);
                 setErrors([]);
-                setIsLoading(false);
             })
             .catch(err => {
-                setIsLoading(false);
                 setErrors(err.response.data.errors);
             });
     }, [pageNumber])
 
 
 
-    return [pictureList, setPictureList, errors, isLoading] as const;
+    return [pictureList, setPictureList, errors] as const;
 }
