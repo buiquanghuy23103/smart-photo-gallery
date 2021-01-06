c4197a7f0d78bce906b7a0d23ad551c4cd23c254
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-01-06 13:39:45 +0200

    Fix: infinite scroll when searching

--- src/utils/hooks/useFetchPicture.tsx
+++ src/utils/hooks/useFetchPicture.tsx
@@ -5,36 +5,22 @@ import { Picture } from '../../types/PictureResponseTypes';
 const BASE_URL = process.env.REACT_APP_UNSPLASH_URL;
 const PUBLIC_KEY = process.env.REACT_APP_UNSPLASH_PUBLIC_KEY;
 
-export default function useFetchPicture(pageNumber: number, query: string | null) {
+export default function useFetchPicture(requestedPageNumber: number, query: string | null) {
     const [pictureList, setPictureList] = useState([] as Picture[]);
     const [errors, setErrors] = useState([] as string[]);
 
-    useEffect(() => {
-        if (!!query) {
-            const url = `${BASE_URL}/search/photos?client_id=${PUBLIC_KEY}&query=${query}`;
-            console.log(url);
-
-            axios.get(url)
-                .then(res => {
-                    setPictureList([...res.data.results]);
-                    setErrors([]);
-                })
-                .catch(err => {
-                    console.log(`errors=${err}`);
-                    setErrors(err.response.data.errors);
-                });
-
-        }
-    }, [query])
-
-    useEffect(() => {
-        if (!!query) return;
-        const url = `${BASE_URL}/photos?client_id=${PUBLIC_KEY}&page=${pageNumber}`;
+    function searchPictures(reset: boolean) {
+        const pageNumber = reset ? 1 : requestedPageNumber;
+        const url = `${BASE_URL}/search/photos?client_id=${PUBLIC_KEY}&query=${query}&page=${pageNumber}`;
         console.log(url);
 
         axios.get(url)
             .then(res => {
-                setPictureList([...pictureList, ...res.data]);
+                if (reset) {
+                    setPictureList([...res.data.results]);
+                } else {
+                    setPictureList([...pictureList, ...res.data.results]);
+                }
                 setErrors([]);
             })
             .catch(err => {
@@ -42,8 +28,45 @@ export default function useFetchPicture(pageNumber: number, query: string | null
                 setErrors(err.response.data.errors);
             });
 
+    }
 
-    }, [pageNumber])
+    function getRandomPictures(reset: boolean) {
+        const pageNumber = reset ? 1 : requestedPageNumber;
+        const url = `${BASE_URL}/photos?client_id=${PUBLIC_KEY}&page=${pageNumber}`;
+        console.log(url);
+
+        axios.get(url)
+            .then(res => {
+                if (reset) {
+                    setPictureList([...res.data]);
+                } else {
+
+                    setPictureList([...pictureList, ...res.data]);
+                }
+                setErrors([]);
+            })
+            .catch(err => {
+                console.log(`errors=${err}`);
+                setErrors(err.response.data.errors);
+            });
+    }
+
+    useEffect(() => {
+        if (!!query) {
+            searchPictures(true);
+        } else {
+            getRandomPictures(true);
+        }
+    }, [query])
+
+    useEffect(() => {
+        if (!!query) {
+            searchPictures(false);
+        } else {
+            getRandomPictures(false);
+        }
+
+    }, [requestedPageNumber])
 
 
 
