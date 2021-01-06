dc3205448640409d9c6e1d3a43aaec2972c31afa
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-01-06 21:19:34 +0200

    Dev: setup Firebase

---
+++ src/config/firebase.tsx
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB-Qy1bgEPuEkvQ0hiLch5c7FCD_I5R5DM",
+    authDomain: "photogallery-6ea49.firebaseapp.com",
+    projectId: "photogallery-6ea49",
+    storageBucket: "photogallery-6ea49.appspot.com",
+    messagingSenderId: "679116635363",
+    appId: "1:679116635363:web:9a893103cdfae7dad799d6"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
