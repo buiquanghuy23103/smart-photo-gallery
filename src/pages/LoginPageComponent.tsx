befff2a437771eb3511591facb1d783a64d9f74a
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-01-06 20:38:29 +0200

    UI: Add more CSS styling to login page

--- src/pages/LoginPageComponent.tsx
+++ src/pages/LoginPageComponent.tsx
@@ -3,27 +3,26 @@ import React from 'react'
 export default function LoginPageComponent() {
     return (
         <div className="flex h-screen bg-gray-200">
-            <div className="m-auto w-1/3 text-white flex flex-wrap justify-center shadow-lg
-            rounded-lg bg-indigo-900">
+            <div className="m-auto w-1/3 text-white flex flex-wrap justify-center shadow-lg rounded-lg bg-gradient-to-br from-indigo-900 to-indigo-700">
                 <form className="m-5 w-10/12">
-                    <h1 className="w-full text-4xl tracking-widest text-center my-6">Login page</h1>
+                    <h1 className="w-full text-4xl tracking-widest text-center">Login</h1>
                     <div className="w-full my-6">
                         <input
                             type="email"
-                            className="p-2 rounded shadow w-full"
+                            className="p-2 rounded shadow w-full text-black"
                             placeholder="Email or username"
                         />
                     </div>
                     <div className="w-full my-6">
                         <input
                             type="password"
-                            className="p-2 rounded shadow w-full"
+                            className="p-2 rounded shadow w-full text-black"
                             placeholder="Password"
                         />
                     </div>
                     <button
                         type="submit"
-                        className="p-2 w-full rounded shadow bg-yellow-400 text-black"
+                        className="p-2 w-full rounded shadow text-black bg-gradient-to-tr from-yellow-600 to-yellow-400"
                     >Login
                     </button>
                 </form>
