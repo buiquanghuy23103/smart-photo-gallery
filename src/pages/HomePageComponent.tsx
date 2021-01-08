427bf04aea9ab4df0050c944dbac21dd8cb0f407
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-01-08 22:38:30 +0200

    UI: add montion to "Welcome Home"

--- src/pages/HomePageComponent.tsx
+++ src/pages/HomePageComponent.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { motion } from 'framer-motion'
+import React from 'react';
 
 export default function HomePageComponent() {
     return (
         <div className="flex h-screen">
-            <h1 className="m-auto text-3xl">Welcome home!</h1>
+            <motion.h1
+                className="m-auto text-3xl"
+                initial={ { opacity: 0, y: -300 } }
+                animate={ {
+                    scale: 2,
+                    opacity: 1,
+                    y: 0,
+                    transition: { duration: 2 },
+                    rotate: 360
+                } }
+            >Welcome home!
+            </motion.h1>
         </div>
     )
 }
