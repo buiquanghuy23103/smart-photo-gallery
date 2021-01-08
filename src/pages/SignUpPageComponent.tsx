b6d72108150d4a6fbf8498ba30869e8a1d5104b4
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-01-08 20:30:06 +0200

    Refactor using Formik context

--- src/pages/SignUpPageComponent.tsx
+++ src/pages/SignUpPageComponent.tsx
@@ -1,4 +1,4 @@
-import { useFormik } from 'formik';
+import { ErrorMessage, Field, Form, Formik, useFormik } from 'formik';
 import { firebaseAuth } from '../config/firebase';
 import { EmailPasswordFormValue } from '../types/Forms';
 import * as Yup from 'yup';
@@ -45,36 +45,51 @@ export default function SignUpPageComponent(): JSX.Element {
     });
 
     return (
-        <div className="flex h-screen bg-gray-200">
-            <div className="m-auto w-1/3 text-white flex flex-wrap justify-center shadow-lg rounded-lg bg-gradient-to-br from-indigo-900 to-indigo-700">
-                <form className="m-5 w-10/12" onSubmit={ formik.handleSubmit } >
-                    <h1 className="w-full text-4xl tracking-widest text-center">Sign up here</h1>
-                    <div className="w-full my-6">
-                        <input
-                            type="email"
-                            className="p-2 rounded shadow w-full text-black"
-                            placeholder="Email or username"
-                            { ...formik.getFieldProps(SignUpFormFields.email) }
-                        />
-                        { showErrorAfterFieldTouched(formik.errors.email) }
-                    </div>
-                    <div className="w-full my-6">
-                        <input
-                            type="password"
-                            className="p-2 rounded shadow w-full text-black"
-                            placeholder="Password"
-                            { ...formik.getFieldProps(SignUpFormFields.password) }
-                        />
-                        { showErrorAfterFieldTouched(formik.errors.password) }
-                    </div>
-                    <button
-                        type="submit"
-                        className="p-2 w-full rounded shadow text-black bg-gradient-to-tr from-yellow-600 to-yellow-400"
-                    >Sign up
+        <Formik
+            initialValues={ {
+                email: '',
+                password: ''
+            } }
+            validateOnChange
+            onSubmit={ handleSubmit }
+            validationSchema={ Yup.object({
+                email: Yup.string().email().required(),
+                password: Yup.string().min(6).required()
+            }) }
+        >
+
+            <div className="flex h-screen bg-gray-200">
+                <div className="m-auto w-1/3 text-white flex flex-wrap justify-center shadow-lg rounded-lg bg-gradient-to-br from-indigo-900 to-indigo-700">
+                    <form className="m-5 w-10/12" onSubmit={ formik.handleSubmit } >
+                        <h1 className="w-full text-4xl tracking-widest text-center">Sign up here</h1>
+                        <div className="w-full my-6">
+                            <Field
+                                type="email"
+                                className="p-2 rounded shadow w-full text-black"
+                                placeholder="Email or username"
+                                name={ SignUpFormFields.email }
+                            />
+                            <ErrorMessage name={ SignUpFormFields.email } />
+                        </div>
+                        <div className="w-full my-6">
+                            <Field
+                                type="password"
+                                className="p-2 rounded shadow w-full text-black"
+                                placeholder="Password"
+                                name={ SignUpFormFields.password }
+                            />
+                            <ErrorMessage name={ SignUpFormFields.password } />
+
+                        </div>
+                        <button
+                            type="submit"
+                            className="p-2 w-full rounded shadow text-black bg-gradient-to-tr from-yellow-600 to-yellow-400"
+                        >Sign up
                     </button>
-                </form>
+                    </form>
+                </div>
             </div>
-        </div>
+        </Formik>
     )
 
 }
